bea2bbac616ef4342118f3c13acfb35745188938
Brayan Mejia Cuenca <mejiabrayan@protonmail.com>
2024-07-26 11:18:43 -0700

    forgot to add adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: cloudflare(),
   output: 'static',
+
   markdown: {
     shikiConfig: {
       theme: 'vitesse-dark',
