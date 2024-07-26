0ff4016e7ce9541f97c232b15ed835a2af8c2587
Brayan Mejia Cuenca <mejiabrayan@protonmail.com>
2024-07-26 11:24:41 -0700

    changed output to hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   adapter: cloudflare(),
-  output: 'static',
+  output: 'hybrid',
 
   markdown: {
     shikiConfig: {
